280f1cbdba66acc9d02be1301d7b389f83aa2b82
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-05 04:23:43 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(arg, noise = "Squawk!")
-  puts "#{arg} #{noise}"
+  puts "#{arg}, #{noise}"
   "Squawk!"
 end
 
