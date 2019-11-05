12f6e76e04ca3b7222327c47dcffee45fd45bbc9
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-05 04:28:58 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(noise = "Squawk!")
+  puts "#{noise}"
+  "#{noise}"
+end
+
+parrot
+
+parrot("Pretty bird!")
